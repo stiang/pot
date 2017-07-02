82297091429cc9cdf11d663982af48519cb1682c
Stian Grytøyr <stian@grytoyr.net>
2017-07-02 08:00:31 +0200

    Update and rename base_32.erl to xbase_32.erl

--- src/xbase_32.erl
+++ src/xbase_32.erl
@@ -17,7 +17,7 @@
 %% under the License.
 %%
 %% -------------------------------------------------------------------
--module(base_32).
+-module(xbase_32).
 -export([encode/1, encode/2, decode/1, decode/2]).
 
 -ifdef(TEST).
